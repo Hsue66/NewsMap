9a25d90ba305aa23f97bd3773ab8c8fdf57bb5a5
hsue66 <dufmaakedl@gmail.com>
2019-02-08 16:21:43 +0900

    modify convert

--- router/convert.js
+++ router/convert.js
@@ -32,17 +32,11 @@ var convert = function(fs){
   }
 
   var sortable = [];
+
   for (var n in nodesNdates) {
       sortable.push([n, nodesNdates[n]]);
   }
-  // console.log("before")
-  // console.log(sortable)
-  // console.log("-----------------------")
-  // sortable.sort(function(a, b) {
-  //     return a[1] - b[1];
-  // });
-
-  console.log(sortable)
+  console.log(nodesNdates)
   /*
   allNodes = Array.from(new Set(allNodes)).sort();
   var nodeNpos = {};
